123961ced0002fca1d455e49f775b9ea822e4c2d
unknown <muhammadyarkhan31@gmail.com>
2026-05-02 15:59:45 +0500

    started code in job.py

---
+++ backend/routes/jobs.py
@@ -0,0 +1,5 @@
+#This file contains all the URL endpoints that the frontend or anyone else
+# can call to get job data. When someone visits /jobs in the browser the
+#code in this file runs and returns job data as JSON.
+
+from fastapi import APIRouter

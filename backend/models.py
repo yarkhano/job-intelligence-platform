8c1108a1d29f6427c27dcc7b560e0e23b5cb3d0a
unknown <muhammadyarkhan31@gmail.com>
2026-05-02 12:52:33 +0500

    Started coding in models.py which represnt our each table using classes

---
+++ backend/models.py
@@ -0,0 +1,5 @@
+# Each class in this file represents one table in your database. SQLAlchemy
+# reads these classes and knows exactly how your tables are structured. These
+# models are used when you want to query or insert data using Python objects ,instead of raw SQL strings.
+
+from sqlalchemy import Column, Integer, String, Float,Date,DateTime,Numeric,SmallInteger,Text

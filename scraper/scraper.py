4f632f83269ea0f849235baa90ae777f6c9392ff
unknown <muhammadyarkhan31@gmail.com>
2026-05-03 20:20:06 +0500

    started writing code in scarper.py impoteed iports

---
+++ scraper/scraper.py
@@ -0,0 +1,6 @@
+from playwright.sync_api import sync_playwright
+from datetime import datetime
+import os
+import time
+from dotenv import load_dotenv
+import psycopg2 #ibrary that connects Python directly to PostgreSQL

6fc193c2f0e8b28b5231df64e585bf32392bad15
unknown <muhammadyarkhan31@gmail.com>
2026-05-03 17:42:54 +0500

    compleetd main.py

--- backend/main.py
+++ backend/main.py
@@ -5,5 +5,22 @@ from fastapi.middleware.cors import CORSMiddleware  #It allows your frontend (ru
 from routes import jobs,applications,users
 from database import Base, engine
 
-Base.metadata.create_all(bind=engine)
-app = FastAPI()
+Base.metadata.create_all(bind=engine) #This tells SQLAlchemy to create any tables that do not exist yet
+app = FastAPI(
+    title = "Job Intelligence Platform API",
+    description = "Backend API for JIP — job matching and analytics",
+    version = "1.0.0"
+)
+
+app.add_middleware(
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.include_router(jobs.router)
+app.include_router(users.router)
+app.include_router(applications.router)
+
+
